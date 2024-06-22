82914ba338368310ed11a2cc06905cbbf8d46b7a
aafisk <axelfisk12@gmail.com>
2024-06-21 21:28:24 -0600

    Initial Commit

---
+++ app/src/main/java/com/fishinator12/questlog/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.fishinator12.questlog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fishinator12.questlog.ui.theme.QuestLogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuestLogTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text("$name!")
+            }
+            ElevatedButton(onClick = {}) {
+                Text("Show more")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp( modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose", "[Name here]")) {
+    Column(modifier) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    QuestLogTheme {
+        MyApp()
+    }
+}
+
+class Quest(var name: String, var category: Int,
+            var weight: Int, var deadline: String,
+            var notes: String) {
+}
