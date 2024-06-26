1ffd075e3e859ff30a95b619a810ddbe2c57e833
aafisk <axelfisk12@gmail.com>
2024-06-25 19:36:30 -0600

    star images added, quest compose complete

--- app/src/main/java/com/fishinator12/questlog/MainActivity.kt
+++ app/src/main/java/com/fishinator12/questlog/MainActivity.kt
@@ -4,19 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fishinator12.questlog.ui.theme.QuestLogTheme
@@ -31,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -39,27 +58,125 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Quest( questName: String, modifier: Modifier = Modifier ) {
+    Surface(color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.clip(RoundedCornerShape(15.dp)).border(0.5.dp, MaterialTheme.colorScheme.onPrimary, RoundedCornerShape(15.dp))
+    ) {
+        Row( modifier = modifier.fillMaxWidth().heightIn(0.dp, 75.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = modifier.padding(horizontal = 16.dp).weight(2f),
+            ) {
+                Text("September 3")
+                Text("3:00 PM")
+            }
+            Text(questName, modifier = modifier.weight(3f))
+            Surface(color = MaterialTheme.colorScheme.secondary, modifier = modifier.weight(0.75f)) {
+                Image(
+                    painter = painterResource(R.drawable.star2),
+                    contentDescription = "stars",
+                    modifier = modifier.fillMaxHeight()
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Quest() {
+    QuestLogTheme {
+        Quest("A task that you should really do already")
+    }
+}
+
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 1.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ), label = "extraPadding"
+    )
+
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Hello ")
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text("Hello ")
                 Text("$name!")
             }
-            ElevatedButton(onClick = {}) {
-                Text("Show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
 
 @Composable
-fun MyApp( modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose", "[Name here]")) {
-    Column(modifier) {
-        for (name in names) {
+fun MyApp( modifier: Modifier = Modifier ) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    QuestLogTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier,
+                     onContinueClicked: () -> Unit
+) {
+    var shouldShowOnboarding = remember { mutableStateOf(true) }
+
+    Column( modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    QuestLogTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
@@ -67,11 +184,12 @@ fun MyApp( modifier: Modifier = Modifier,
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun GreetingPreview() {
+fun GreetingsPreview() {
     QuestLogTheme {
-        MyApp()
+        Greetings()
     }
 }
+
 
 class Quest(var name: String, var category: Int,
             var weight: Int, var deadline: String,
--- app/src/main/java/com/fishinator12/questlog/ui/theme/Theme.kt
+++ app/src/main/java/com/fishinator12/questlog/ui/theme/Theme.kt
@@ -9,18 +9,21 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LGray,
+    secondary = Cream,
+    background = Color(0xFFE8DEC8)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LGray,
+    secondary = Cream,
+    background = Color(0xFFE8DEC8),
+//    tertiary = Pink40,
+    onPrimary = Yellow
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
